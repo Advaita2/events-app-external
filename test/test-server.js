07428432cb145a9284bb71050ef2cd7815c49508
Advaita2 <advaita.halvadiya@gmail.com>
2022-11-04 18:14:31 +0000

    testfix

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Advaita'S application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Advaita's application</h1>"));
         return done();
       });
 
